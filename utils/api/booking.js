9af2e85eed82fc2ba2b42d4d5797e51db08e1cc3
Yusfi Krismon Valas <yusfi.krismon@ai.astra.co.id>
2026-04-10 23:42:45 +0700

    Add whole API form heroku, then making a new load test fonr create read delete journey, but still have an issue about 403 forbidden in get booking by id

--- utils/api/booking.js
+++ utils/api/booking.js
@@ -1,6 +1,25 @@
 import http from 'k6/http';
 import { check } from 'k6';
-import { BASE_URL, DEFAULT_HEADERS } from '../../config/options.js';
+import { BASE_URL, DEFAULT_HEADERS, AUTH_HEADERS } from '../../config/options.js';
+import { SharedArray } from 'k6/data';
+
+const bookingData = new SharedArray('bookings', function () {
+  return JSON.parse(open('../../utils/data/createBooking.json'));
+});
+
+function bookingPayload(data) {
+  return JSON.stringify({
+    firstname: data.firstname,
+    lastname: data.lastname,
+    totalprice: data.totalprice,
+    depositpaid: data.depositpaid,
+    bookingdates: {
+      checkin: data.checkin,
+      checkout: data.checkout,
+    },
+    additionalneeds: data.additionalneeds,
+  });
+}
 
 // GET /booking — ambil semua booking ID
 export function getBookingIds() {
@@ -12,6 +31,12 @@ export function getBookingIds() {
     'getBookingIds - status 200': (r) => r.status === 200,
     'getBookingIds - has data': (r) => r.json().length > 0,
   });
+
+
+  if (res.status !== 200) {
+    console.warn(`getBookingById failed - status: ${res.status}, body: ${res.body}`);
+    return null;
+  };
 
   return res.json();
 }
@@ -27,6 +52,115 @@ export function getBookingById(bookingId) {
     'getBookingById - has firstname': (r) => r.json('firstname') !== null,
     'getBookingById - has lastname': (r) => r.json('lastname') !== null,
   });
+
+  if (res.status !== 200) {
+    console.warn(`getBookingById failed - status: ${res.status}, body: ${res.body}`);
+    return null;
+  };
+
+  return res.json();
+}
+
+// POST /booking — buat booking baru
+export function createBooking() {
+
+  const data = bookingData[Math.floor(Math.random() * bookingData.length)];
+  const createBookingPayload = bookingPayload(data);
+
+  const res = http.post(
+    `${BASE_URL}/booking`,
+    createBookingPayload,
+    {
+      headers: DEFAULT_HEADERS,
+    }
+  );
+
+  check(res, {
+    'createBooking - status 200': (r) => r.status === 200,
+    'createBooking - has bookingid': (r) => r.json('bookingid') !== null,
+  });
+
+  if (res.status !== 200) {
+    console.warn(`getBookingById failed - status: ${res.status}, body: ${res.body}`);
+    return null;
+  };
+
+  return res.json();
+}
+
+export function updateBooking(bookingId, token) {
+
+  const data = bookingData[Math.floor(Math.random() * bookingData.length)];
+
+  const updateBookingPayload = bookingPayload(data);
+
+  const res = http.put(
+    `${BASE_URL}/booking/${bookingId}`,
+    updateBookingPayload,
+    {
+      headers: AUTH_HEADERS(token),
+    }
+  );
+
+  check(res, {
+    'updateBooking - status 200': (r) => r.status === 200,
+    'updateBooking - has firstname': (r) => r.json('firstname') === data.firstname,
+    'updateBooking - has lastname': (r) => r.json('lastname') === data.lastname,
+  });
+
+
+  if (res.status !== 200) {
+    console.warn(`getBookingById failed - status: ${res.status}, body: ${res.body}`);
+    return null;
+  };
+
+  return res.json();
+}
+
+export function deleteBooking(bookingId, token) {
+  const res = http.del(
+    `${BASE_URL}/booking/${bookingId}`,
+    null,
+    {
+      headers: AUTH_HEADERS(token),
+    }
+  );
+
+  check(res, {
+    'deleteBooking - status 201': (r) => r.status === 201,
+  });
+  
+}
+
+export function updateBookingPartially(bookingId, token) {
+  
+  const data = bookingData[Math.floor(Math.random() * bookingData.length)];
+
+
+  const updateBookingPayload = JSON.stringify({
+    firstname: data.firstname,
+    lastname: data.lastname,
+  });
+
+  const res = http.patch(
+    `${BASE_URL}/booking/${bookingId}`,
+    updateBookingPayload,
+    {
+      headers: AUTH_HEADERS(token),
+    }
+  );
+
+  check(res, {
+    'updateBookingPartially - status 200': (r) => r.status === 200,
+    'updateBookingPartially - has firstname': (r) => r.json('firstname') === data.firstname,
+    'updateBookingPartially - has lastname': (r) => r.json('lastname') === data.lastname,
+  });
+
+
+  if (res.status !== 200) {
+    console.warn(`getBookingById failed - status: ${res.status}, body: ${res.body}`);
+    return null;
+  };
 
   return res.json();
 }
